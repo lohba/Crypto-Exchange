e49e19e98b22e14e5a951fffae0d99c1fbbe7b23
Luke Ohba <lohba@Lukes-MacBook-Pro.local>
2021-01-01 11:42:07 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -21,10 +21,12 @@ function App() {
         </tr>
       </thead>
       <tbody>
-        <Coin name="Bitcoin" ticker="BTC" price="$9999"/>
-        <Coin name="Ethereum" ticker = "ETH" price="$299.99"/>    
+        <Coin name="Bitcoin" ticker="BTC" price={9999}/>
+        <Coin name="Ethereum" ticker = "ETH" price={299.99}/>
+        <Coin name="Tether" ticker="USDT" price="1.0"/>    
+        <Coin name= "Ripple" ticker="XRP" price={0.2}/>
       </tbody>
-    </table>
+    </table> 
     </div>
   );
 }
